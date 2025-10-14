a04063312b69c59f6267f159510c40fbd69d6f18
admfireandsecurity <chloemmoore99@gmail.com>
2025-10-14 15:06:56 +0100

    Updated Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./Navbar.css"; // Optional: for styling
+import "./Navbar.css";
 
 const Navbar: React.FC = () => {
   return (
@@ -8,21 +8,11 @@ const Navbar: React.FC = () => {
       <div className="logo">
         <Link to="/">ADM Fire & Security</Link>
       </div>
-      <ul className="nav-links">
-        <li><Link to="/">Home</Link></li>
-        <li className="dropdown">
-          <span>Services</span>
-          <ul className="dropdown-content">
-            <li><Link to="/service-fire">Fire Alarm Installation</Link></li>
-            <li><Link to="/service-cctv">CCTV Installation</Link></li>
-            <li><Link to="/service-access">Access Control</Link></li>
-            <li><Link to="/service-intruder">Intruder Alarms</Link></li>
-          </ul>
-        </li>
-        <li><Link to="/contact">Contact</Link></li>
-      </ul>
-    </nav>
-  );
-};
-
-export default Navbar;
+      <div className="nav-links">
+        <Link to="/">Home</Link>
+        <div className="services-dropdown">
+          <span>Services ▼</span>
+          <div className="services-dropdown-content">
+            <Link to="/service-fire">Fire Alarm Installation</Link>
+            <Link to="/service-cctv">CCTV Installation</Link>
+            <Li
