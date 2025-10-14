baaedab5b139a9742c10f92aac85bb7bf3fa7209
admfireandsecurity <chloemmoore99@gmail.com>
2025-10-14 13:30:49 +0100

    Updated Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom"; // assuming you are using react-router-dom
 
 const Navbar: React.FC = () => {
   return (
-    <nav className="navbar">
-      <div className="nav-logo">ADM Fire & Security</div>
+    <nav>
+      <h1>ADM Fire & Security</h1>
       <ul className="nav-links">
-        <li><a href="#services">Services</a></li>
-        <li><a href="#contact">Contact</a></li>
+        <li><Link to="/">Home</Link></li>
+        <li className="services-dropdown">
+          <span>Services</span>
+          <ul className="dropdown-menu">
+            <li><Link to="/service-fire">Fire Alarm Installation</Link></li>
+            <li><Link to="/service-cctv">CCTV Installation</Link></li>
+            <li><Link to="/service-access">Access Control</Link></li>
+            <li><Link to="/service-intruder">Intruder Alarm</Link></li>
+          </ul>
+        </li>
+        <li><Link to="/contact">Contact</Link></li>
       </ul>
     </nav>
   );
