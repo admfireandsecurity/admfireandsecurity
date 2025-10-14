823d0ea0e1084e85e7530706366ec154ae470b9f
admfireandsecurity <chloemmoore99@gmail.com>
2025-10-14 12:24:27 +0100

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,26 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import Services from "./components/Services";
-import Contact from "./components/Contact";
 import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import ServiceFire from "./pages/ServiceFire";
+import ServiceCCTV from "./pages/ServiceCCTV";
+import ServiceIntruder from "./pages/ServiceIntruder";
+import ServiceLighting from "./pages/ServiceLighting";
 
 const App: React.FC = () => {
   return (
     <>
       <Navbar />
-      <Hero />
-      <Services />
-      <Contact />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/services/fire-alarms" element={<ServiceFire />} />
+        <Route path="/services/cctv" element={<ServiceCCTV />} />
+        <Route path="/services/intruder-alarms" element={<ServiceIntruder />} />
+        <Route path="/services/emergency-lighting" element={<ServiceLighting />} />
+      </Routes>
       <Footer />
     </>
   );
