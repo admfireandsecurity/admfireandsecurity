d1c833f0424baca30dc094c0801d62ebbda0b10d
admfireandsecurity <chloemmoore99@gmail.com>
2025-10-14 12:12:13 +0100

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import React from "react";
+import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Services from "./components/Services";
 import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 const App: React.FC = () => {
   return (
-    <div>
+    <>
+      <Navbar />
       <Hero />
       <Services />
       <Contact />
-    </div>
+      <Footer />
+    </>
   );
 };
 
